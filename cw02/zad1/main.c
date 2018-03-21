89d6ea438dc8d91d99f486e17e985cfa6ed8e0b4
Kamil Świerad <kam.swierad@gmail.com>
2018-03-21 02:10:36 +0100

    Working zad2, needs refining

--- cw02/zad1/main.c
+++ cw02/zad1/main.c
@@ -6,8 +6,8 @@ int main(){
 
     //for(int i=0;i<256;i++) printf("%c %d |",i,i);
 
-    generate_file("test.txt",10,5);
-    copy_file("test.txt","test2.txt",10,5);
-    sort_file("test.txt",10,5);
+    generate_file("test.txt",1500,50);
+    copy_file("test.txt","test2.txt",1500,50);
+    sort_file("test.txt",1500,50);
     return 0;
 }
